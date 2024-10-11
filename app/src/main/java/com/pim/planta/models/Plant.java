22dcba5ab30c91c4d8c417454082afd825f9b617
RamonAvocado <ramonet.miret@gmail.com>
2024-10-11 18:50:47 +0200

    Ideas de algunos metodos y creacion plant

---
+++ app/src/main/java/com/pim/planta/models/Plant.java
@@ -0,0 +1,10 @@
+package com.pim.planta.models;
+
+public class Plant {
+
+    public Plant(int xp, int xp_max, String plant_name, String image_url){
+        xp = 0;
+        xp_max = 1000000;
+        plant_name = "Vinicius";
+    }
+}
